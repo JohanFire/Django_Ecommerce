f7cb8d3bff43942c59826b9f4825e24420e67cf7
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-10-19 23:20:57 -0500

    Auto filling 'slug' field with the 'category_name' while creating new Catergory in Django admin dashboard

--- ecommerce_django/category/admin.py
+++ ecommerce_django/category/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Category
 
 class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("category_name",)} # auto fill slug with category_name. Replacing " " with "-"
     list_display = ("category_name", "slug")
 
 # Register your models here.
