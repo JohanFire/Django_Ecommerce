2d69bdc19d8a64db3de12acd93d0266d3fb3426e
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-10-22 23:09:45 -0500

    Search products by Category ( store/slug )

--- ecommerce_django/store/views.py
+++ ecommerce_django/store/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Product
+from category.models import Category
 
 # Create your views here.
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+def store(request, category_slug=None):
+    # show products per Category
+    categories = None
+    products = None
+
+    if category_slug != None:
+        # if don't get the object with parameters defined, will show 404
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+        
     context = {
         "products": products,
         "product_count": product_count,
