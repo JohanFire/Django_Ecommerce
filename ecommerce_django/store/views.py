4fad38dec6280e787d59571daf7000f6f78f4427
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-10-23 21:20:46 -0500

    Search products in navbar

--- ecommerce_django/store/views.py
+++ ecommerce_django/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 from .models import Product
 from category.models import Category
@@ -52,3 +53,21 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, "store/product_detail.html", context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET["keyword"]
+        
+        if keyword:
+            products = Product.objects.order_by("-created_date").filter(
+                Q(description__icontains=keyword) | Q(product_name__icontains=keyword)
+                # order_by("-...") means descending order
+                # .filter( if description contains keyword OR product_name contains keyword)
+            ) 
+            product_count = products.count()
+    context = {
+        "products": products,
+        "product_count": product_count,
+    }
+
+    return render(request, "store/store.html", context)
